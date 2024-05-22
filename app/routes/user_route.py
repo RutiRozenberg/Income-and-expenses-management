2b7da790d776c3d5a8a233c1fbbe9c1a212e365f
RutiRozenberg <ruti7633@gmail.com>
2024-05-22 20:05:29 +0300

    fix functions in user_service file

--- app/routes/user_route.py
+++ app/routes/user_route.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException
 
 from app.models.User import User
-from app.services.user_service import signIn_service, signUp_service
+from app.services.user_service import signIn_service, signUp_service, update_user_service
 
 user_router = APIRouter()
 
@@ -19,3 +19,15 @@ async def signUp(user: User):
     if not await signUp_service(user):
         raise HTTPException(status_code=400, detail="oops... invalid parameters")
     return True
+
+
+@user_router.put("/put/{email}")
+async def update_user(email: str, user_from_body: User):
+    if await update_user_service(email, user_from_body):
+        return True
+    raise HTTPException(status_code=404, detail="oops...")
+
+
+
+
+
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,4 +1,5 @@
 import bcrypt
+from fastapi import HTTPException
 from pydantic import ValidationError
 
 from app.db_management.user_collection import *
@@ -22,8 +23,15 @@ async def delete_user(email: str):
     await delete_user_db(email)
 
 
-async def update_user(email: str, user: User):
-    await update_user_db(email, user)
+async def update_user_service(email: str, user_from_body: User):
+    if user_from_body.email != email:
+        raise HTTPException(status_code=400, detail="oops... try another email")
+    user: User = await get_by_email(email)
+    if user.name != user_from_body.name:
+        user.name = user_from_body.name
+    if user.password != hashPassword(user_from_body.password):
+        user.password = user_from_body.password
+    return await update_user_db(email, user)
 
 
 async def signIn_service(email: str, password: str):
@@ -37,11 +45,14 @@ async def signIn_service(email: str, password: str):
 
 
 async def signUp_service(newUser: User):
+    oldUser = await get_by_email(newUser.email)
+    if oldUser:
+        raise HTTPException(status_code=400, detail="oops... invalid email")
     try:
         newUser.password = hashPassword(newUser.password)
         await save_user_db(newUser)
         return True
-    except ValidationError as error:
+    except ValidationError:
         return False
 
 
